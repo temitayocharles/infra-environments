ddaf5e2518280f952766dcd362004b61f6902b15
Temitayo Charles Akinniranye <s9charles.wft@gmail.com>
2026-03-01 02:23:30 -0500

    Pin bootstrap stack to stable secret module

--- environments/staging/stacks/bootstrap/main.tf
+++ environments/staging/stacks/bootstrap/main.tf
@@ -1,7 +1,7 @@
 module "argocd_bootstrap" {
   count = try(local.env.modules_enabled.argocd_bootstrap, false) ? 1 : 0
 
-  source = "git::https://github.com/temitayocharles/terraform-module.git//module/argocd-bootstrap?ref=v1.1.2"
+  source = "git::https://github.com/temitayocharles/terraform-module.git//module/argocd-bootstrap?ref=v1.1.3"
 
   argocd_bootstrap_config = local.env.argocd_bootstrap_config
   repo_passwords          = var.argocd_repo_passwords
